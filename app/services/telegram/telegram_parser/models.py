7757b94d1c3357957f97e5f343ffe1044404ae41
Nic11371 <nic11371@gmail.com>
2025-05-21 23:14:21 +0300

    create_parser-11-fix

--- app/services/telegram/telegram_parser/models.py
+++ app/services/telegram/telegram_parser/models.py
@@ -4,5 +4,27 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
-class Message(models.Model):
-    pass
+class Vacancy(models.Model):
+    name = models.CharField(
+        max_length=255, verbose_name=('vacancy_name')
+    )
+    company = models.CharField(
+        max_length=255, verbose_name=('company')
+    )
+    city = models.CharField(
+        max_length=255, verbose_name=('city')
+    )
+    salary = models.CharField(
+        max_length=255, verbose_name=('salary')
+    )
+    date = models.DateTimeField(verbose_name=('date'))
+    link = models.CharField(
+        max_length=255, verbose_name=('link')
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("=Vacation=")
+        verbose_name_plural = _("=Vacation=")
--- app/services/telegram/telegram_parser/views.py
+++ app/services/telegram/telegram_parser/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from django_filters.views import FilterView
 from .filters import MessageFilter
-from .models import Message
+from .models import Vacancy
 
 # Create your views here.
 
