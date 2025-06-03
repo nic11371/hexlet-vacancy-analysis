a634962682c80b90b2cd479f718762dac201fcaf
Lugovskikh Konstantin <kostya.lugovskikh@mail.ru>
2025-06-03 12:15:05 +0500

    create branch and first settings

---
+++ app/services/hh/hh_parser/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HhParserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app.services.hh.hh_parser'
--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.services.telegram.telegram_parser'
+    'app.services.hh.hh_parser',
 ]
 
 MIDDLEWARE = [
