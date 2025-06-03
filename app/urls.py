8d1bfa9a581c57e15bfb81e60c67d6f7dc9dce9e
Lugovskikh Konstantin <kostya.lugovskikh@mail.ru>
2025-06-03 12:21:14 +0500

    update urls

--- app/urls.py
+++ app/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hh', include('app.services.hh.hh_parser.urls')),
+    path('hh/', include('app.services.hh.hh_parser.urls')),
 ]
