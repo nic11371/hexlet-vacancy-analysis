3012e23dcf5cb591eba07ff44d3de2fa23ae4d89
Danil Mullakhmetov <danilmullakhmetov@gmail.com>
2025-07-23 15:48:55 +0300

    feat: add basic model registration of hh_parser in the admin panel

--- app/services/hh/hh_parser/admin.py
+++ app/services/hh/hh_parser/admin.py
@@ -1,3 +1,35 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Vacancy
 
-# Register your models here.
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'company_name',
+        'city',
+        'salary',
+        'experience',
+        'published_at',
+    )
+    search_fields = ('title', 'company_name', 'city', 'key_skills')
+    list_filter = ('city', 'experience', 'schedule', 'employment')
+    ordering = ('-published_at',)
+    readonly_fields = ('created_at',)
+
+    fieldsets = (
+        ('Description', {
+            'fields': (
+                'title', 'company_name', 'url', 'salary', 'experience',
+                'employment', 'work_format', 'schedule', 'key_skills', 'description',
+            )
+        }),
+        ('Lokation', {
+            'fields': ('city', 'street', 'building'),
+            'classes': ('collapse',)
+        }),
+        ('Additional information', {
+            'fields': ('contacts', 'published_at', 'created_at'),
+            'classes': ('collapse',)
+        }),
+    )
