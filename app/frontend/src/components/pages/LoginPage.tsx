cbe66e57a0e33c9680bdbebbc5d015c79adcb2a0
JuliePro <prokofevajuliya@gmail.com>
2025-09-10 08:44:49 +0300

    Feat: transition to a new architecture Inertia.js

--- app/frontend/src/components/pages/LoginPage.tsx
+++ app/frontend/src/components/pages/LoginPage.tsx
@@ -1,8 +1,7 @@
 const LoginPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Login</h1>
-      <p className="mt-4">Информация.</p>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl">Вход в аккаунт</h1>
     </div>
   );
 };
