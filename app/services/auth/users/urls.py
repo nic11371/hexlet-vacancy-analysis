8cecbb24d0f178ae2d70a2a341c90e283d15eed0
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-09-15 11:47:58 +0400

    refactor: remove yandex and github authentication routes from user URLs

--- app/services/auth/users/urls.py
+++ app/services/auth/users/urls.py
@@ -12,6 +12,4 @@ urlpatterns = [
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
     path('csrf/', views.get_csrf_token, name='csrf'),
-    path("yandex/", include("app.services.auth.yandex_id.urls")),
-    path("github/", include("app.services.auth.github.urls")),
 ]
