634b5afade5638b6ca3005e54169d6aef7010456
Nikolay Melnikov <71149100+nic11371@users.noreply.github.com>
2025-09-29 08:36:08 +0300

    Update apps.py
    fix name app

--- app/services/profession/apps.py
+++ app/services/profession/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ProfessionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'profession'
+    name = 'app.services.profession'
