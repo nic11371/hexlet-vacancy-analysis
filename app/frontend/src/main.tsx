730a95ebaee0e4d75c5a2fc71e6d25a32b7f5cf7
ShominDaniil <d.sh.v@yandex.ru>
2025-10-18 17:19:27 +0300

    feat: fix build to render

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,26 +1,21 @@
-import "vite/modulepreload-polyfill";
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import { InertiaProgress } from '@inertiajs/progress';
 import axios from 'axios';
 import React from 'react';
-document.addEventListener('DOMContentLoaded', () => {
 
-    const csrfToken = document.querySelector('meta[name=csrf-token]').getAttribute('content');
+document.addEventListener('DOMContentLoaded', () => {
+    const csrfMeta = document.querySelector('meta[name=csrf-token]');
+    const csrfToken = csrfMeta ? csrfMeta.getAttribute('content') : '';
     axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
 
     InertiaProgress.init({ color: '#4B5563' });
 
     createInertiaApp({
         resolve: (name) => import(`./components/pages/${name}.tsx`),
-        setup({ el, App, props }: {
-            el: HTMLElement,
-            App: React.ComponentType<{ page: any }>,
-            props: any
-        }) {
+        setup: ({ el, App, props }) => {
             const root = createRoot(el);
-            root.render(<App {...props} />);
+            root.render(React.createElement(App, props));
         },
     });
-
 });
