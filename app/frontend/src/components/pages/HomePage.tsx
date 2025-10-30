280f37e2554430d1fd1c5f75298e5491cb3fb0c2
Konstantin Shagan <konstantinshagan@gmail.com>
2025-10-30 16:32:21 +0300

    deleted auxilary code

--- app/frontend/src/components/pages/HomePage.tsx
+++ app/frontend/src/components/pages/HomePage.tsx
@@ -1,37 +1,8 @@
-import { VacancyCard } from "../shared/VacancyCard";
-import { usePage } from "@inertiajs/react";
-import type { VacancyCardProps } from "../../types";
-import type React from "react";
-import { Card } from "@mantine/core";
-
-interface HomePageProps {
-  vacancies: VacancyCardProps[];
-  [key: string]: any;
-}
-
-const HomePage: React.FC = () => {
-  const { props } = usePage<HomePageProps>()
-  console.log(props.vacancies);
+const HomePage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Главная</h1>
       <p className="mt-4">Информация.</p>
-      <Card>
-        {props.vacancies.map((vacancy) => (
-          <Card key={vacancy.id}>
-              <VacancyCard 
-              id={vacancy.id}
-              title={vacancy.title}
-              salary={vacancy.salary}
-              company={vacancy.company}
-              city={vacancy.city}
-              employment={vacancy.employment}
-              skills={vacancy.skills}
-              url={vacancy.url}
-            />
-          </Card>
-        ))}
-      </Card>
     </div>
   );
 };
--- app/views.py
+++ app/views.py
@@ -1,48 +1,12 @@
 from django.http import JsonResponse
 from inertia import render as inertia_render
-# Для себя 
-from app.services.hh.hh_parser.models import Vacancy
-
 
 def index(request):
-    vacancies_qs = Vacancy.objects.select_related('company', 'city').all()
-
-    vacancies = []
-    for v in vacancies_qs:
-        vacancy_data = {
-            'id': v.id,
-            'title': v.title,
-            'url': v.url,
-            'salary': v.salary or '',
-            'experience': v.experience or '',
-            'employment': v.employment or '',
-            'company': {
-                'id': v.company.id if v.company else None,
-                'name': v.company.name if v.company else '',
-            },
-            'city': {
-                'id': v.city.id if v.city else None,
-                'name': v.city.name if v.city else '',
-            } if v.city else None,
-            'skills': v.skills.split(',') if v.skills else [],
-        }
-        vacancies.append(vacancy_data)
-
     return inertia_render(
         request,
         "HomePage",
-        props={
-            'vacancies': vacancies  # передаем массив вакансий во фронтенд
-        }
+        props={},
     )
-# Для себя 
-
-# def index(request):
-#     return inertia_render(
-#         request,
-#         "HomePage",
-#         props={},
-#     )
 
 
 def custom_server_error(request):
