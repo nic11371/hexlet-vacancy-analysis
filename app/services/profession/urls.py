92bafccbae509692427116c148a5cb07b1a15754
Nikolay <nic11371@gmail.com>
2025-11-11 22:12:09 +0300

    update inertia

--- app/services/profession/urls.py
+++ app/services/profession/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ProfessionView
 
 urlpatterns = [
-    path("<slug:profession_slug>/", ProfessionView, name='profession_page')
+    path("<slug:profession_slug>/", ProfessionView.as_view(), name='profession_page')
 ]
